23aef331d9ff2cfcfa201f815a5d227ea86c525e
z4gon <4588601+z4gon@users.noreply.github.com>
2024-04-12 01:52:14 -0300

    slugify on save

--- app/models.py
+++ app/models.py
@@ -1,19 +1,42 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Post(models.Model):
+    """
+    A model to represent a blog post.
+    """
+    
+    # auto_now_add=True
+    #     means that the field will be automatically set to now when the object is first created.
+    #     https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.DateField.auto_now_add
+
+    # auto_now=True
+    #     means that the field will be automatically set to now every time the object is saved.
+    #     https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.DateField.auto_now
+
+    # blank=True
+    #     means that the field is allowed to be blank, like an empty string.
+    #     https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.Field.blank
+
+    # null=True
+    #     means that the field is allowed to be null.
+    #     https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.Field.null
+
     title = models.CharField(max_length=200)
     body = models.TextField()
-    # auto_now_add=True means that the field will be automatically set to now when the object is first created.
-    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.DateField.auto_now_add
     created_at = models.DateTimeField(auto_now_add=True) 
-    # auto_now=True means that the field will be automatically set to now every time the object is saved.
-    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.DateField.auto_now
     updated_at = models.DateTimeField(auto_now=True) 
     slug = models.SlugField(max_length=200, unique=True)
-    # blank=True means that the field is allowed to be blank, like an empty string.
-    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.Field.blank
-    # null=True means that the field is allowed to be null.
-    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.Field.null
     image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    # https://docs.djangoproject.com/en/4.2/topics/db/models/#overriding-model-methods
+    def save(self, *args, **kwargs):
+        if not self.id: # only on creation
+            self.slug = slugify(self.title)
+
+        return super().save(*args, **kwargs)
