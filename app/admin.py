2aecf2243078d4e5f55bab17cad1c09cd28eaab7
z4gon <4588601+z4gon@users.noreply.github.com>
2024-04-12 01:51:53 -0300

    admin model for post

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'content')
+    exclude = ('slug',)
+
+admin.site.register(Post, PostAdmin)
