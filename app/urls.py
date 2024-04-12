b4e89c7988bd3e368661534e084203b3b54d5cc6
z4gon <4588601+z4gon@users.noreply.github.com>
2024-04-12 20:38:06 -0300

    show posts for a tag

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.posts_list, name='posts_list'),
     path('post/<slug:post_slug>', views.post_details, name='post_details'),
+    path('tag/<slug:tag_slug>', views.tag_posts_list, name='tag_posts_list'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from app.models import Post
+from app.models import Post, Tag
 
 # Create your views here.
 
@@ -15,3 +15,12 @@ def post_details(request, post_slug):
         return render(request, 'app/404.html', status=404)
 
     return render(request, 'app/post_details.html', {'post': post})
+
+def tag_posts_list(request, tag_slug):
+    try:
+        tag = Tag.objects.get(slug=tag_slug)
+    except Tag.DoesNotExist:
+        return render(request, 'app/404.html', status=404)
+    
+    posts = tag.posts.all()[0:10]
+    return render(request, 'app/tag_posts_list.html', {'tag': tag, 'posts': posts})
