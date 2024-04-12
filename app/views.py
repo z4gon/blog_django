62f9579e7699e40c6ac19db25b3f4dca10942fc3
z4gon <4588601+z4gon@users.noreply.github.com>
2024-04-12 01:52:49 -0300

    views for posts and post

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from app.models import Post
+
 # Create your views here.
+
+def posts(request):
+    posts = Post.objects.all()[0:10]
+    return render(request, 'app/posts.html', {'posts': posts})
+
+def post(request, post_slug):
+    try:
+        post = Post.objects.get(slug=post_slug)
+    except Post.DoesNotExist:
+        return render(request, 'app/404.html', status=404)
+
+    return render(request, 'app/post.html', {'post': post})
