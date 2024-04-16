29dfb50064dd2142173a1d8d74bd0a9419861c6d
z4gon <4588601+z4gon@users.noreply.github.com>
2024-04-16 04:50:39 -0300

    posts related name on author

--- app/models.py
+++ app/models.py
@@ -72,7 +72,7 @@ class Post(models.Model):
 
     # relationships
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True, related_name='posts')
 
     def __str__(self):
         return self.title
